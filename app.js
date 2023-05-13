0e6232b5a14dc0f50c02c14d5196e3a2c407c7a7
Vishnu Vardhan <vishnuvardhan15503@gmail.com>
2023-05-13 11:42:00 +0530

    Level 4 Security(Salting+Hashing)

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import {Schema, model} from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -34,35 +36,41 @@ app.get('/register',(req,res)=>{
 });
 
 app.post('/register', async (req,res)=>{
-    const newUser = {
-        email : req.body.username,
-        password : md5(req.body.password)
-    };
-    await User.create(newUser).then((val)=>{
-        console.log("New user added to the database...");
-        res.render('secrets');
-        return;
-    }).catch((err)=>{
-        console.log(err);
-        res.send("Error occured while adding the user to the database!!!");
-        return;
+    const username = req.body.username;
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds).then(async function(hash) {
+        // Store hash in your password DB.
+        const newUser = {
+            email : username,
+            password : hash
+        };
+        await User.create(newUser).then((val)=>{
+            console.log("New user added to the database...");
+            res.render('secrets');
+            return;
+        }).catch((err)=>{
+            console.log(err);
+            res.send("Error occured while adding the user to the database!!!");
+            return;
+        });
     });
 });
 
 app.post('/login', async (req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     await User.findOne({email:username}).then((val)=>{
-        if(val.password === password){
-            res.render('secrets');
-            return;
+        if(val){
+            bcrypt.compare(password, val.password).then(function(result) {
+                if(result === true) res.render('secrets');
+                else res.send("Wrong password!!!");
+            });
         }else{
-            res.send("Incorrect Password!!!");
+            res.send("User not found!!!");
             return;
         }
     }).catch((err)=>{
         console.log(err);
-        res.send("User not found!!!");
         return;
     });
 });

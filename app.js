82031517a7c3dc91072e5eccd318ee3568115396
Vishnu Vardhan <vishnuvardhan15503@gmail.com>
2023-05-16 00:21:19 +0530

    Level 5 Security(Cookies and Sessions)

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import {Schema, model} from "mongoose";
-import bcrypt from "bcrypt";
-
-const saltRounds = 10;
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema =  new Schema({
@@ -17,11 +30,14 @@ const userSchema =  new Schema({
     password : String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new model("User",userSchema);
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); 
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -35,44 +51,55 @@ app.get('/register',(req,res)=>{
     res.render('register');
 });
 
-app.post('/register', async (req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    bcrypt.hash(password, saltRounds).then(async function(hash) {
-        // Store hash in your password DB.
-        const newUser = {
-            email : username,
-            password : hash
-        };
-        await User.create(newUser).then((val)=>{
-            console.log("New user added to the database...");
-            res.render('secrets');
-            return;
-        }).catch((err)=>{
-            console.log(err);
-            res.send("Error occured while adding the user to the database!!!");
-            return;
-        });
+app.get('/secrets',(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout',(req,res)=>{
+    req.logout((err)=>{
+        if (err) { 
+          return next(err); 
+          }
+        res.redirect('/');
     });
 });
 
-app.post('/login', async (req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    await User.findOne({email:username}).then((val)=>{
-        if(val){
-            bcrypt.compare(password, val.password).then(function(result) {
-                if(result === true) res.render('secrets');
-                else res.send("Wrong password!!!");
-            });
-        }else{
-            res.send("User not found!!!");
-            return;
-        }
+app.post('/register', async (req,res)=>{
+
+    User.register({username:req.body.username},req.body.password).then((val)=>{
+        console.log(val);
+        passport.authenticate("local")(req,res,()=>{
+            res.redirect('/secrets');
+        });
     }).catch((err)=>{
         console.log(err);
-        return;
+        res.redirect('/register');
     });
+    
+});
+
+app.post('/login', async (req,res)=>{
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
+
+    req.login(user,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local")(req,res,()=>{
+                res.redirect('/secrets');
+            });
+        }
+    });
+
 });
 
 app.listen(3000,()=>{
